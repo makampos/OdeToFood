2ca54560c005c29da8762b8287c9eb9d32399cb9
makampos <matheus.campos@edu.unifil.br>
2020-08-03 23:50:34 -0300

    update

---
+++ OdeToFood.Data/OdeToFoodDbContext.cs
@@ -0,0 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+using OdeToFood.Core;
+
+namespace OdeToFood.Data
+{
+    public class OdeToFoodDbContext : DbContext
+    {
+        public DbSet<Restaurant> Restaurants { get; set; }
+    }
+}
